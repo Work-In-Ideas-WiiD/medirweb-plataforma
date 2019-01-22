8109c3fbcaf0d385b588170f0904ca88ef75adcc
charles <charlesegidio@gmail.com>
2019-01-22 17:59:24 -0200

    ajuste ciclo numero 3

--- app/Models/Leitura.php
+++ app/Models/Leitura.php
@@ -37,8 +37,8 @@ class Leitura extends Model
             ->join('unidades', 'unidades.UNI_ID', '=', 'prumadas.PRU_IDUNIDADE')
             ->join('imoveis', 'imoveis.IMO_ID', '=', 'unidades.UNI_IDIMOVEL')
             ->where('imoveis.IMO_ID', '=', $value)
-            ->where('leituras.created_at', '>=', date('2019-01-22').' 16:28:09')
-            ->where('leituras.created_at', '<=', date('2019-01-22').' 16:39:08')
+            ->where('leituras.created_at', '>=', date('2019-01-22').' 17:13:18')
+            ->where('leituras.created_at', '<=', date('2019-01-22').' 17:21:50')
             ->orderBy('unidades.UNI_NOME', 'asc')
             ->get();
     }
