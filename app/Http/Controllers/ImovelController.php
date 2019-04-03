6c5cc23c266b1610f4df4fa7aa4ebc95aa4492f2
Iury Nascimento <i.v.nascimento@hotmail.com>
2019-04-03 18:19:49 -0300

    API. TESTE

--- app/Http/Controllers/ImovelController.php
+++ app/Http/Controllers/ImovelController.php
@@ -349,7 +349,7 @@ class ImovelController extends Controller
 
 
 
-        setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.UTF-8', 'portuguese');
+        /*setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.UTF-8', 'portuguese');
         date_default_timezone_set('America/Sao_Paulo');
 
         $teste = ucwords(strftime('%B %Y', strtotime('2019-03-03')));
@@ -360,7 +360,7 @@ class ImovelController extends Controller
         phpinfo();
 
 
-        die;
+        die;*/
 
 
 
