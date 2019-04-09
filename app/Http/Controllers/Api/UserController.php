2423439543ff805f99f69df923090ca396e5d779
Iury Nascimento <i.v.nascimento@hotmail.com>
2019-04-09 14:07:02 -0300

    API. Email incorreto

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -59,7 +59,7 @@ class UserController extends Controller
             $message->from('suporte@medirweb.com.br', 'MedirWeb - Plataforma individualizadora');
             $message->to($user->email);
             $message->cc('linconaraujo@medirweb.com.br');
-            $message->cc('i.v.nascimento@gmail.com');
+            $message->cc('i.v.nascimento.ti@gmail.com');
             $message->subject('Senha de acesso ao app');
         });
 
