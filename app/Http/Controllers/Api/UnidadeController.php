afcd9bf6720af1f490867f768366390d4f8ef4c4
Iury Nascimento <i.v.nascimento.ti@gmail.com>
2019-05-21 18:58:36 -0300

    import User no api/unidadeController

--- app/Http/Controllers/Api/UnidadeController.php
+++ app/Http/Controllers/Api/UnidadeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Unidade;
+use App\User;
 
 class UnidadeController extends Controller
 {
