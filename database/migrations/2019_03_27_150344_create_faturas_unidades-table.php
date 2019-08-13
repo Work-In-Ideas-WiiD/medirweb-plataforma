94a199bc8ec414f7fb2967de6923df1c724dd32a
Cosmo André <cosmo_moraes@hotmail.com>
2019-08-13 17:49:47 -0300

    novo ajuste na migração

--- database/migrations/2019_03_27_150344_create_faturas_unidades-table.php
+++ database/migrations/2019_03_27_150344_create_faturas_unidades-table.php
@@ -28,9 +28,20 @@ class CreateFaturasUnidadesTable extends Migration
                 ->on('faturas')
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
-            $table->string('valor_total', 255);
+            $table->decimal('valor', 22,2)
+                ->comment('aqui fica o valor da prumada');
 
-            $table->longText('FATUNI_PRUMADAS');
+            $table->unsignedBigInteger('prumada_id');
+            $table->foreign('prumada_id')
+                ->references('id')
+                ->on('prumadas')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->bigInteger('prumada_consumo');
+            $table->bigInteger('prumada_leitura_anterior')->default(0);
+            $table->date('prumada_data_leitura_anterior');
+            $table->bigInteger('prumada_leitura_atual');
+            $table->date('prumada_data_leitura_atual');
 
             $table->softDeletesTz();
             $table->timestampsTz();
