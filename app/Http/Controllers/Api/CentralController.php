56b68f41b591ba718d8708e0326015a66f141745
charles <charlesegidio@gmail.com>
2020-01-27 13:09:23 -0300

    dd teste

--- app/Http/Controllers/Api/CentralController.php
+++ app/Http/Controllers/Api/CentralController.php
@@ -374,10 +374,10 @@ class CentralController extends Controller
 
     private function _imovelConsumoEstimado($unidade)
     {
-        return $this->_imovelConsumoConsolidado($unidade, [
-            'data1' => now()->day(1)->format('Y-m-d'),
-            'data2' => now()->format('Y-m-d')
-        ]);
+        // return $this->_imovelConsumoConsolidado($unidade, [
+        //     'data1' => now()->day(1)->format('Y-m-d'),
+        //     'data2' => now()->format('Y-m-d')
+        // ]);
 
         dd($this->_imovelConsumoConsolidado($unidade, [
             'data1' => now()->day(1)->format('Y-m-d'),
