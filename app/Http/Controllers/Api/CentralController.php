d735e05d0e992275602c219a9fc47d41970a58a0
charles <charlesegidio@gmail.com>
2020-07-02 08:55:23 -0300

    somente um teste payload

--- app/Http/Controllers/Api/CentralController.php
+++ app/Http/Controllers/Api/CentralController.php
@@ -458,11 +458,11 @@ class CentralController extends Controller
 
     public function webhook(Request $request)
     {
-        file_put_contents(storage_path('app/requestall.txt'), json_encode(request()->all()));
+        file_put_contents(storage_path('app/requestall'.now()->format('Y-m-d H:i').'.txt'), json_encode(request()->all()));
 
-        file_put_contents(storage_path('app/payloads.txt'), $request->payloads);
+        file_put_contents(storage_path('app/payloads'.now()->format('Y-m-d H:i').'.txt'), $request->payloads);
 
-        file_put_contents(storage_path('app/payload.txt'), $request->payload);
+        file_put_contents(storage_path('app/payload'.now()->format('Y-m-d H:i').'.txt'), $request->payload);
 
         return ['retorno' =>  'OK', 'status' => true];
     }
